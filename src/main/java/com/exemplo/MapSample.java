36fec7d28791e1f371a9c0051c96225fdb044d5f
marcantonioo <dinizsilvam3@gmail.com>
2026-04-13 16:33:44 -0300

    Fix: Correção executeOnEntries
    Troca de Lambda por herdeira de EntryProcessor na chamada de executeOnEntries arquivo MapSample, metodo Sample3.

--- src/main/java/com/exemplo/MapSample.java
+++ src/main/java/com/exemplo/MapSample.java
@@ -1,72 +1,67 @@
-package com.exemplo;
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.map.EntryProcessor;
-import com.hazelcast.map.IMap;
+  package com.exemplo;
+  import com.hazelcast.client.HazelcastClient;
+  import com.hazelcast.client.config.ClientConfig;
+  import com.hazelcast.core.Hazelcast;
+  import com.hazelcast.core.HazelcastInstance;
+  import com.hazelcast.map.EntryProcessor;
+  import com.hazelcast.map.IMap;
 
-import java.util.*;
+  import java.util.*;
 
-public class MapSample {
-  
-  public void Sample(){
-    ClientConfig clientConfig = new ClientConfig();
-    clientConfig.setClusterName("hello-world"); 
-
-    HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig); 
-    IMap <String,String> map = client.getMap("my-distributed-map"); 
-
+  public class MapSample {
     
-    map.put("1", "John");
-    map.put("2", "Mary");
-    map.put("3", "Jane");
-  }
-  public void Sample2(){
+    public void Sample(){
+      ClientConfig clientConfig = new ClientConfig();
+      clientConfig.setClusterName("hello-world"); 
 
-    ClientConfig clientConfig = new ClientConfig();
-    clientConfig.setClusterName("hello-world");
+      HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig); 
+      IMap <String,String> map = client.getMap("my-distributed-map"); 
 
-    HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
-
-    
-    IMap <String,String> map = client.getMap("my-distributed-map");
-    for (Map.Entry<String, String> entry : map.entrySet()) {
-      System.out.println(entry.getKey() + " " + entry.getValue());
+      
+      map.put("1", "John");
+      map.put("2", "Mary");
+      map.put("3", "Jane");
     }
+    public void Sample2(){
 
-    client.shutdown(); 
-  }
-  public void Sample3(){
+      ClientConfig clientConfig = new ClientConfig();
+      clientConfig.setClusterName("hello-world");
 
-    ClientConfig clientConfig = new ClientConfig();
-    clientConfig.setClusterName("hello-world");
+      HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
 
-    HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+      
+      IMap <String,String> map = client.getMap("my-distributed-map");
+      for (Map.Entry<String, String> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + " " + entry.getValue());
+      }
 
-    
-    IMap <String,String> map = client.getMap("my-distributed-map");
-    for (Map.Entry<String, String> entry : map.entrySet()) {
-      System.out.println(entry.getKey() + " " + entry.getValue());
+      client.shutdown(); 
     }
-    map.executeOnEntries((EntryProcessor<String, String, String>)entry -> {
-      System.out.println("Executando em: " + Hazelcast.getHazelcastInstanceByName("dev"));
-      String updatedValue = entry.getValue() + "f";
-      entry.setValue(updatedValue);
-      return null;
-    }); 
-    for (Map.Entry<String, String> entry : map.entrySet()) {
-      System.out.println(entry.getKey() + " " + entry.getValue());
+    public void Sample3(){
+
+      ClientConfig clientConfig = new ClientConfig();
+      clientConfig.setClusterName("hello-world");
+
+      HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+
+      
+      IMap <String,String> map = client.getMap("my-distributed-map");
+      for (Map.Entry<String, String> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + " " + entry.getValue());
+      }
+      Map<String, String> res = map.executeOnEntries( new IncrementingEntryProcessor() ); 
+      for (Map.Entry<String, String> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + " " + entry.getValue());
+      }
+
+      client.shutdown(); 
     }
-
-    client.shutdown(); 
   }
-}
 
 
-/*map.executeOnEntries((EntryProcessor<Integer, String, String>)entry -> {
-      System.out.println("Executando em: " + Hazelcast.getHazelcastInstanceByName("dev"));
-      String updatedValue = entry.getValue() + "f";
-      entry.setValue(updatedValue);
-      return updatedValue;
-    }); */
+  /*map.executeOnEntries((EntryProcessor<Integer, String, String>)entry -> {
+        System.out.println("Executando em: " + Hazelcast.getHazelcastInstanceByName("dev"));
+        String updatedValue = entry.getValue() + "f";
+        entry.setValue(updatedValue);
+        return updatedValue;
+      }); */
